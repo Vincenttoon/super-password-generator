77da95ae8501a52f6bad8f74ab7858c5454ee62c
Vincent Toon <vincenttoon22@gmail.com>
2022-08-01 17:59:43 -0500

    oh boy where do I even start

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,46 @@
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+    var promptNumbers = window.prompt("Would you like UPPERCASE letters in your password? Please type YES or NO.");
+
+    if (promptNumbers === "YES") {
+      keys.uppercase();
+    }
+    
+    if (promptNumbers === "NO") {
+      return false;
+    }
+
+    promptNumbers = promptNumbers.toLowerCase();
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+};
+
+writePassword();
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate").addEventListener("submit", function()) {
+
+};
+
 // Assignment code here
 
 const keys = {
-  letter: "ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-  number: "0123456789"
-  symbol: "!@#$%^&*()_+~\`|}{[]:;?><,./-=",
+  uppercase: "ABCDEFGHIJKLMOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()_+~|}{[]:;?><,./-=",
 };
 
 const getKey = [
-  function letter() {
-    return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+  function uppercase() {
+    return keys.uppercase[Math.floor(Math.random() * keys.uppercase.length)];
+  },
+  function lowercase() {
+    return keys.lowercase[Math.floor(Math.random() * keys.lowercase.length)];
   },
   function number() {
     return keys.number[Math.floor(Math.random() * keys.number.length)];
@@ -17,18 +49,3 @@ const getKey = [
     return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
   }
 ];
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate")
