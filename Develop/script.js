aa4273a3e0bd7c3fb6579378baba23f12bc46e13
Vincent Toon <vincenttoon22@gmail.com>
2022-08-01 18:34:35 -0500

    a fresh start

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,20 @@
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Hey! You clicked the button!");
+
+  return "Generated Password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-    var promptNumbers = window.prompt("Would you like UPPERCASE letters in your password? Please type YES or NO.");
-
-    if (promptNumbers === "YES") {
-      keys.uppercase();
-    }
-    
-    if (promptNumbers === "NO") {
-      return false;
-    }
-
-    promptNumbers = promptNumbers.toLowerCase();
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 };
 
-writePassword();
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate").addEventListener("submit", function()) {
-
-};
-
-// Assignment code here
-
-const keys = {
-  uppercase: "ABCDEFGHIJKLMOPQRSTUVWXYZ",
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  number: "0123456789",
-  symbol: "!@#$%^&*()_+~|}{[]:;?><,./-=",
-};
-
-const getKey = [
-  function uppercase() {
-    return keys.uppercase[Math.floor(Math.random() * keys.uppercase.length)];
-  },
-  function lowercase() {
-    return keys.lowercase[Math.floor(Math.random() * keys.lowercase.length)];
-  },
-  function number() {
-    return keys.number[Math.floor(Math.random() * keys.number.length)];
-  },
-  function symbol() {
-    return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-  }
-];
+generateBtn.addEventListener("click", writePassword); 
