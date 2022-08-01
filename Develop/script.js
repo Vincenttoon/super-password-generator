c7ee4f255804ffeab64be657c6a7822d14617dae
Vincent Toon <vincenttoon22@gmail.com>
2022-08-01 18:38:29 -0500

    psuedo code

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,14 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Hey! You clicked the button!");
 
+// 1. Prompt User for the password criteria
+//  a. Password length 8 < 128
+//  b. Lowercase, uppercase, numbers, special characters
+
+// 2. Validate the input
+
+// 3.Display password on the page
+
   return "Generated Password will go here!";
 }
 
