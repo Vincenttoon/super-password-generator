afc4c70022b17774c8bbe0eb284a42eb737185fa
Vincent Toon <vincenttoon22@gmail.com>
2022-08-02 17:54:47 -0500

    if and else prompts in for test

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,31 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("Hey! You clicked the button!");
-  
+
+    // prompt for length of password
   lengthOfPwd = window.prompt("How long would you like your password to be? Please choose between 8 - 128 characters.");
   if (lengthOfPwd < 8 || lengthOfPwd > 128 ) {
-    return "Please choose a character length between 8 and 128.";
+    // returns user to length of window prompt
+    return "Please choose a character length between 8 and 128.";\
+    // confirms length of lengthOfPwd
   } else { 
       window.alert("Your password will be" + lenghtOfPwd + "characters.");
+  }
+
+  // confirm window for lower case letters, using confirm to only allow 2 possible choices
+  lowerCaseCheck = window.confirm("Would you like to include lower case letters?");
+  if (lowerCaseCheck) {
+    window.alert = ("Your password WILL include lower case letters!");
+  } else {
+    window.alert = ("Your password WILL NOT include lower case letters.");
+  }
+
+  // confirm window for upper case letter, same thought process as above
+  upperCaseCheck = window.confirm("Would you like to include upper case letters?");
+  if (upperCaseCheck) {
+    window.alert = ("Your password WILL include upper case letters!");
+  } else {
+    window.alert = ("Your password WILL NOT include upper case letters.");
   }
 
   console.log(lengthOfPwd);
