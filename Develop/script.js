27fcdb2a682dc7a0a03855b8461367e57a0f7ff5
Vincent Toon <vincenttoon22@gmail.com>
2022-08-02 21:22:54 -0500

    alerts now work, no spaces

--- Develop/script.js
+++ Develop/script.js
@@ -19,7 +19,7 @@ function generatePassword() {
     return "Please choose a character length between 8 and 128.";
     // confirms length of lengthOfPwd
   } else { 
-    window.alert = (" Your password will be " + lengthOfPwd + " characters. ");
+    window.alert(" Your password will be " + lengthOfPwd + " characters. ");
   }
 
   // confirm window for lower case letters, using confirm to only allow 2 possible choices
@@ -27,31 +27,31 @@ function generatePassword() {
   if (lowerCaseCheck) {
     var lowerCaseCheckYes = window.alert("Your password WILL include lower case letters!");
   } else {
-    window.alert = ("Your password WILL NOT include lower case letters.");
+    window.alert("Your password WILL NOT include lower case letters.");
   }
 
   // confirm window for upper case letter, same thought process as above
   upperCaseCheck = window.confirm("Would you like to include upper case letters?");
   if (upperCaseCheck) {
-    window.alert = ("Your password WILL include upper case letters!");
+    window.alert("Your password WILL include upper case letters!");
   } else {
-    window.alert = ("Your password WILL NOT include upper case letters.");
+    window.alert("Your password WILL NOT include upper case letters.");
   }
 
   // Same previous two, but for numbers
   numberCheck = window.confirm("Would you like to include numbers?");
   if (numberCheck = true) {
-    window.alert = ("Your password WILL include upper case letters!");
+    window.alert("Your password WILL include upper case letters!");
   } else {
-    window.alert = ("Your password WILL NOT include numbers.");
+    window.alert("Your password WILL NOT include numbers.");
   }
 
   // Same as previous three, but for special characters
   symbolCheck = window.confirm ("Would you like to include special characters?");
   if (symbolCheck) {
-    window.alert = ("Your password WILL include special characters!");
+    window.alert("Your password WILL include special characters!");
   } else {
-    window.alert = ("Your password WILL NOT include special characters.");
+    window.alert("Your password WILL NOT include special characters.");
   }
 
   // use concat to assign value to password string
